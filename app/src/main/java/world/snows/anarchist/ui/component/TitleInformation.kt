1830577f36cad18ac0bda20f2aad8b813285ca78
David Long <david.long@snows.world>
2024-01-07 22:32:51 -0500

    Delete unused component

--- app/src/main/java/world/snows/anarchist/ui/component/TitleInformation.kt
+++
@@ -1,9 +0,0 @@
-package world.snows.anarchist.ui.component
-
-import androidx.compose.runtime.Composable
-import world.snows.anarchist.SearchResultQuery
-
-@Composable
-fun TitleInformation(titleData: SearchResultQuery.Medium) {
-
-}
